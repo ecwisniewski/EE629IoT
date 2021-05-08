17b90473a37fad49cdab9818f3e8ebbddbf80a82
errski <69652270+errski@users.noreply.github.com>
2021-05-08 12:22:46 -0400

    Work on website for system
    Some stylistic things and CC icons from iconmonstr.com to use  on webpage.

--- Plant Monitoring System/Web/index.php
+++ Plant Monitoring System/Web/index.php
@@ -2,10 +2,19 @@
   <head>
     <meta charset="utf-8">
     <title>Plant Monitoring Data</title>
-  </head>
+    <link rel="icon" href="favicon.ico"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Amaranth&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
   <body>
     <h1>Plant Monitoring System (EE629-Spring 2021)</h1>
     <?php echo '<p>This is PHP</p>'; ?>
-    <?php phpinfo(); ?>
+    <hr />
+    <h1>Plant Data Info</h1>
+    <hr>
+    <!--
+    <h1>Graphic Data</h1>
+    <?php //phpinfo(); ?>-->
   </body>
 </html>
