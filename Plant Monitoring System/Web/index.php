bf58465e2906a4358a4c7f667b8a62861531a8b9
errski <69652270+errski@users.noreply.github.com>
2021-05-08 11:33:34 -0400

    Added php file for plant monitoring website

--- Plant Monitoring System/Web/index.php
+++ Plant Monitoring System/Web/index.php
@@ -1,9 +1,11 @@
-<html lang="en" dir="ltr">
+<html>
   <head>
     <meta charset="utf-8">
     <title>Plant Monitoring Data</title>
   </head>
   <body>
     <h1>Plant Monitoring System (EE629-Spring 2021)</h1>
-      </body>
+    <?php echo '<p>This is PHP</p>'; ?>
+    <?php phpinfo(); ?>
+  </body>
 </html>
