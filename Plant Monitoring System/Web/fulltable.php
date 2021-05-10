51ffa785cce72d81682024e0d9955e385bd02d12
errski <69652270+errski@users.noreply.github.com>
2021-05-09 23:01:24 -0400

    Fixed PHP files for web page with Apache

---
+++ Plant Monitoring System/Web/fulltable.php
@@ -0,0 +1,60 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Plant Monitoring Data</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Amaranth&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <?php
+      $username = "pi";
+      $password = "password"; // not real password
+      $database = "database";
+    ?>
+    <!-- Title Div -->
+    <div class="title">
+      <img class="plant" src="images/plant.png" alt="plant image">
+      <h1 class="plant">Plant Monitoring System (EE629-Spring 2021)</h1>
+      <img class="plant" src="images/plant.png" alt="plant image">
+    </div>
+    <hr align="left">
+    <!-- Database -->
+    <div class="full-data">
+      <h2>Plant Data Full Info</h2>
+      <?php
+        $mysqli = new mysqli("localhost", $username, $password, $database);
+        $query = "SELECT * FROM testtable order by tdate desc, ttime desc limit 60";
+        echo '<table style="width:1000px">
+              <tr>
+                <td><b>Date</b></td>
+                <td><b>Time</b></td>
+                <td><b>Moisture</b></td>
+                <td><b>Light</b></td>
+                <td><b>Temperature</b></td>
+                <td><b>Humidity</b></td>
+              </tr>';
+
+          if ($result=$mysqli->query($query)) {
+            while($row = $result->fetch_assoc()) {
+
+              echo '<tr>
+                      <td>' . $row["tdate"] . '</td>
+                      <td>' . $row["ttime"] . '</td>
+                      <td>' . $row["moisture"] . '</td>
+                      <td>' . $row["light"] . '</td>
+                      <td>' . $row["temperature"] . 'F</td>
+                      <td>' . $row["humidity"] . '%</td>
+                    </tr>';
+            }
+
+            $result->free();
+          }
+
+          echo '</table>';
+        ?>
+      </div>
+    </div>
+  </body>
+</html>
--- Plant Monitoring System/Web/index.php
+++ Plant Monitoring System/Web/index.php
@@ -6,8 +6,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Amaranth&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
-</head>
+  </head>
   <body>
+    <!-- PHP to connect MySQL
+        Get info from the latest row -->
     <?php
       // Threshold Values
       $light_thresh_1 = 7;
@@ -20,7 +22,7 @@
       $database = "test";
 
       $mysqli = new mysqli("localhost", $username, $password, $database);
-      $query = "SELECT * FROM testtable order by tdate desc limit 1";
+      $query = "SELECT * FROM testtable order by tdate desc, ttime desc limit 1";
       if ($result=$mysqli->query($query)) {
         $lastrow = $result->fetch_assoc();
         $dispmoisture = $lastrow["moisture"];
@@ -29,109 +31,95 @@
         $disphumidity = $lastrow["humidity"];
         $result->free();
       }
-
     ?>
 
+    <!-- Title Div -->
     <div class="title">
-
       <img class="plant" src="images/plant.png" alt="plant image">
       <h1 class="plant">Plant Monitoring System (EE629-Spring 2021)</h1>
       <img class="plant" src="images/plant.png" alt="plant image">
-      </div>
-      <hr>
-      <div class="monitor">
-
-
+    </div>
+    <hr align="left">
+    <!-- Monitor - Contains Icons and (Small) Database Table -->
+    <div class="monitor">
       <div class="visual">
         <div class="sunlight">
           <h2>Brightness</h2>
+          <!-- Display icon dependant on brightness-->
           <?php
-          if($displight >$light_thresh_2) {
-            echo '<img src="images/brightday3.png" alt="brightness indicator image">';
-          }
-          else if($displight > $light_thresh_1) {
-            echo '<img src="images/brightday2.png" alt="brightness indicator image">';
-
-          }
-          else {
-            echo '<img src="images/brightday1.png" alt="brightness indicator image">';
-
-          }
+            if($displight >$light_thresh_2) {
+              echo '<img src="images/brightday3.png" alt="brightness indicator image">';
+            }
+            elseif($displight > $light_thresh_1) {
+              echo '<img src="images/brightday2.png" alt="brightness indicator image">';
+            }
+            else {
+              echo '<img src="images/brightday1.png" alt="brightness indicator image">';
+            }
           ?>
         </div>
-
-      <div class="water">
-          <h2>Moisture</h2>
-          <?php
-          if($dispmoisture >$moisture_thresh_2) {
-            echo '<img src="images/water3.png" alt="water indicator image">';
-          }
-          else if($dispmoisture > $moisture_thresh_1) {
-            echo '<img src="images/water2.png" alt="water indicator image">';
-
-          }
-          else {
-            echo '<img src="images/water1.png" alt="water indicator image">';
-
-          }
-          ?>
-      </div>
-
+        <div class="water">
+            <h2>Moisture</h2>
+            <!-- Display icon dependant on moisture-->
+            <?php
+              if($dispmoisture >$moisture_thresh_2) {
+                echo '<img src="images/water3.png" alt="water indicator image">';
+              }
+              elseif($dispmoisture > $moisture_thresh_1) {
+                echo '<img src="images/water2.png" alt="water indicator image">';
+              }
+              else {
+                echo '<img src="images/water1.png" alt="water indicator image">';
+              }
+            ?>
+        </div>
         <br>
         <div class="temphum">
           <h2>Temperature and Humidity</h2>
           <img src="images/temp.png" alt="temp indicator image">
           <div class="temphuminfo">
+            <!-- Print Temperature and Humidity-->
             <?php
-              echo '<p>
-              '. $disptempurature . ' F<br />' . $disphumidity . '
-               %</p>'
-             ?>
+              echo '<p>'. $disptempurature . ' F<br />' . $disphumidity . ' %</p>'
+            ?>
           </div>
         </div>
-
       </div>
-      <!--<hr>-->
+
+      <!-- Data -->
       <div class="data">
         <h2>Plant Data Info</h2>
+        <!-- Display Small table from MySQL database-->
+        <?php
+          $mysqli = new mysqli("localhost", $username, $password, $database);
+          $query = "SELECT * FROM testtable order by tdate desc, ttime desc limit 20";
+          echo '<table>
+                <tr>
+                  <td><b>Date</b></td>
+                  <td><b>Time</b></td>
+                  <td><b>Moisture</b></td>
+                  <td><b>Light</b></td>
+                  <td><b>Temperature</b></td>
+                  <td><b>Humidity</b></td>
+                </tr>';
+          if ($result=$mysqli->query($query)) {
+            while($row = $result->fetch_assoc()) {
+              echo '<tr>
+                      <td>' . $row["tdate"] . '</td>
+                      <td>' . $row["ttime"] . '</td>
+                      <td>' . $row["moisture"] . '</td>
+                      <td>' . $row["light"] . '</td>
+                      <td>' . $row["temperature"] . 'F</td>
+                      <td>' . $row["humidity"] . '%</td>
+                    </tr>';
+            }
 
-
-      <?php
-      $query = "SELECT * FROM testtable";
-      echo '<table>
-              <tr>
-                <td><b>Date</b></td>
-                <td><b>Time</b></td>
-                <td><b>Moisture</b></td>
-                <td><b>Light</b></td>
-                <td><b>Temperature</b></td>
-                <td><b>Humidty</b></td>
-              </tr>';
-      if ($result = $mysqli->query($query)) {
-         while ($row = $result->fetch_assoc()) {
-
-              $date = $row["tdate"]
-              $time = $row["ttime"]
-              $moisture = $row["moisture"]
-              $light = $row["light"]
-              $tempurature = $row["temperature"]
-              $humidity = $row["humidity"]
-      	echo '<tr>
-              <td>' . $date . '</td>
-              <td>' . $time . '</td>
-              <td>' . $moisture . '</td>
-              <td>' . $light . '</td>
-              <td>' . $tempurature . 'F</td>
-              <td>' . $humidity . '%</td>
-              </tr>';
+            $result->free();
           }
-          $result->free();
-      }
-      echo '</table>';
-    ?>
-  </div>
 
-  </div>
-    <hr>
+          echo '</table>';
+        ?>
+      </div>
+    </div>
   </body>
 </html>
